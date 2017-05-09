f0ce62c204f31d90099bf8e4e209f28512f9d76b
Michael Cittar <mcittar@gmail.com>
2017-05-09 10:44:30 -0700

    Babel compiles both react and css correctly

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,9 +1,20 @@
+// Import the page's CSS. Webpack will know what to do with it.
+import "../stylesheets/app.css";
+
+var Web3 = require("web3");
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './container.jsx';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const root = document.getElementById('root');
+  if (typeof web3 !== 'undefined') {
+    window.web3 = new Web3(web3.currentProvider);
+  } else {
+    window.web3 = new Web3(
+      new Web3.providers.HttpProvider("http://localhost:8545")
+    );
+  }
+  const root = document.getElementById('root');
 
-    ReactDOM.render(<App />, root);
-  });
+  ReactDOM.render(<App />, root);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: './app/javascripts/app.js',
@@ -11,7 +12,8 @@ module.exports = {
     // Copy our app's index.html to the build folder.
     new CopyWebpackPlugin([
       { from: './app/index.html', to: "index.html" }
-    ])
+    ]),
+    new ExtractTextPlugin('static/css/[name].[contenthash:8].css')
   ],
   resolve: {
     extensions: ['.js', '.jsx', '*']
@@ -24,18 +26,24 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015'],
+          presets: ['es2015'],
           plugins: ['transform-runtime']
         }
       },
+    ],
+    rules: [
       {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015']
-        }
-      }
+       test: /\.css$/,
+       use: [ 'style-loader', 'css-loader' ]
+     },
+     {
+       test: /\.jsx?$/,
+       exclude: /(node_modules|bower_components)/,
+       loader: 'babel-loader',
+       query: {
+         presets: ['react', 'es2015']
+       }
+     },
     ]
-  }
+  },
 };
