1fb33ac989f2d5e3eed214d2bcdc7b944b1a8626
Michael Cittar <mcittar@gmail.com>
2017-05-09 13:47:05 -0700

    Create root react component

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,14 +1,17 @@
+// Import the page's CSS. Webpack will know what to do with it.
+import "../stylesheets/app.css";
 import { default as Web3} from 'web3';
-import conference_artifacts from '../../build/contracts/Conference.json';
+import conferenceArtifacts from '../../build/contracts/Conference.json';
 import { default as contract } from 'truffle-contract';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './container.jsx';
+import Container from './container.jsx';
 
-const Conference = contract(conference_artifacts);
-let web3;
+const Conference = contract(conferenceArtifacts);
 
 document.addEventListener('DOMContentLoaded', () => {
+  let web3;
+  let conferenceContract;
   if (typeof web3 !== 'undefined') {
     web3 = new Web3(web3.currentProvider);
   } else {
@@ -21,23 +24,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
   Conference.new({ from: accounts[0], gas: 4712388 }).then(
     function(conference) {
-
+    conferenceContract = conference;
+    const root = document.getElementById('root');
+    ReactDOM.render(<Container Conference={ conferenceContract } />, root);
     var ticketPrice = web3.toWei(.05, 'ether');
     var initialBalance = web3.eth.getBalance(conference.address).toNumber();
-    console.log("The conference's initial balance is: " + initialBalance);
-
-    conference.buyTicket({ from: accounts[1], value: ticketPrice }).then(
-      function() {
-        var newBalance = web3.eth.getBalance(conference.address).toNumber();
-        console.log("After someone bought a ticket it's: " + newBalance);
-        return conference.refundTicket(accounts[1], ticketPrice, {from: accounts[0]});
-      }).then(
-        function() {
-          var balance = web3.eth.getBalance(conference.address).toNumber();
-          console.log("After a refund it's: " + balance);
-      });
   });
 
-  const root = document.getElementById('root');
-  ReactDOM.render(<App />, root);
 });
--- app/javascripts/container.jsx
+++ app/javascripts/container.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
 
-class App extends React.Component {
+
+class Container extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      quota: 500
+    };
+  }
+
+  componentDidMount(){
+    this.props.Conference.quota.call().then((quota) => {
+      this.setState({ quota: quota.toNumber() });
+    });
   }
 
   render() {
+    this.props.Conference.quota.call().then((quota) => {
+      console.log(quota.toNumber());
+			return this.props.Conference.organizer.call();
+	}).then((organizer) => {
+      console.log(organizer);
+			return this.props.Conference.numRegistrants.call();
+	}).then((num) => {
+      console.log(num.toNumber());
+	});
     return (
       <div className='app'>
-        SOME TEXT
+        { this.props.Conference.address }<br></br>
+        { this.state.quota }
       </div>
     );
   }
 
 }
 
-export default App;
+export default Container;
